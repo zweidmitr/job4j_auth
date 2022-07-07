f7da496af0410c0fcf2bae0137e543eea081f8ca
zweidmitr <zweidmitr@gmail.com>
2022-07-07 15:33:58 +0300

    Настройка проекта

---
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAuthApplication.class, args);
+    }
+}
---
+++ src/main/resources/db/update_001.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS person (
+    id SERIAL PRIMARY KEY ,
+    login VARCHAR(2000),
+    password VARCHAR(2000)
+);
+
+INSERT INTO person(login, password) VALUES ('parsentev','123');
+INSERT INTO person(login, password) VALUES ('ban','123');
+INSERT INTO person(login, password) VALUES ('ivan','123');
